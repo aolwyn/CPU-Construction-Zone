86401b3518486a766ecb6f5a31acbb8f026a23b2
Adam Bayley <94658867+aolwyn@users.noreply.github.com>
2022-02-15 23:55:41 -0500

    Update rotate_L.v

--- rotate_L.v
+++ rotate_L.v
@@ -15,9 +15,9 @@ end module
 	/* below is corrected version, pending check
 `timescale 1ns / 1ps
 
-module rotateLeft(output out[31:0], input in[31:0])
+module rotateLeft(output out[31:0], input in[31:0]);
 
-assign out = {in[0],in[31:1]}
+assign out = {in[0],in[31:1]};
 
 
 endmodule    

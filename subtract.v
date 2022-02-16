354fa3045088b5bed73610027b74d9f3370d1598
Adam Bayley <94658867+aolwyn@users.noreply.github.com>
2022-02-15 23:44:06 -0500

    Update subtract.v

--- subtract.v
+++ subtract.v
@@ -1,7 +1,7 @@
-module subtract(in1, in2, out);
+module subtract(output out[31:0], input in1[31:0], in2[31:0]);
 
 input in1, in2
 output out
 assign out = in1 - in2
 
-endmodule :subtract;
+endmodule 

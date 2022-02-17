97ef2abe6b944c3f08cf661bb0648cb60596be8b
Samantha <18srh5@queensu.ca>
2022-02-16 23:56:24 -0500

    Divide

--- divide_32.v
+++ divide_32.v
@@ -1,6 +1,40 @@
 `timescale 1ns / 1ps
 
-module division(clk, dividend, divisor, Q, R, ready);
+module divide(input [31:0] dividend, divisor, output reg [31:0] quotient);
+    reg [31:0] m, q;
+    reg [32:0] a;
+    integer i;
+
+    always @ (*)
+    begin
+        begin
+        q = dividend;
+        m = divisor;
+        a = 0;
+        for(i = 0; i < 32; i = i+1)
+        begin
+            a = {a[30:0], q[31]};
+            q[31:1] = q[30:0];
+            a = a - m;
+
+            if(a[31] == 1)
+            begin
+                q[0] = 0;
+                a = a + m;
+            end
+            else
+            begin
+                q[0] = 1;
+            end
+        end
+        quotient = q;
+          end
+    end 
+
+endmodule
+
+
+/*module division(clk, dividend, divisor, Q, R, ready);
 
 input clk;
 input [15:0] dividend, divisor;
@@ -8,8 +42,6 @@ output[15:0] Q, R;
 output ready;
 
 reg[31:0] copyDivi, copyDivis, difference;
-
-
 reg[31:0] bit;
 reg [15:0] Q;
 
@@ -45,7 +77,7 @@ end
 endmodule
  
 
-
+*/
 
 /*
 module divide_32 (

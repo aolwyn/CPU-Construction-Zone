621d52c7ed64f74e670556820517afad3f7db7f1
Samantha <18srh5@queensu.ca>
2022-03-14 14:14:27 -0400

    To do

--- PCincrement.v
+++ PCincrement.v
@@ -1,17 +1,20 @@
 `timescale 1ns / 1ps
 
 module PCincrement #(parameter qInitial = 0)(
-	input clk, IncPC,
-	input [31:0] inputPC,
-	output reg[31:0] newPC
-	);
-	
+    input clk, clr, en, IncPC,
+    input [31:0] inputPC,
+    output reg[31:0] newPC
+    );
+
 initial newPC = qInitial;
-	
+
 always @ (posedge clk)
-	begin
-		if(IncPC == 1)
-			newPC <= newPC + 1;
-	end
-				
+    begin
+        if(clr)
+            newPC = 0;
+        else if (en)
+            newPC = inputPC +1;
+
+    end
+
 endmodule

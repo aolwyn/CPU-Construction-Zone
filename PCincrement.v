3bcdd0174f928b22348ea6dbc3b25073ea4889e2
Adam Bayley <94658867+aolwyn@users.noreply.github.com>
2022-03-14 13:54:22 -0400

    updated PC increment for working version.

--- PCincrement.v
+++ PCincrement.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 
 module PCincrement #(parameter qInitial = 0)(
-	input clk, IncPC,
+	input clk, IncPC, enable,
 	input [31:0] inputPC,
 	output reg[31:0] newPC
 	);
@@ -10,8 +10,10 @@ initial newPC = qInitial;
 	
 always @ (posedge clk)
 	begin
-		if(IncPC == 1)
+		if(IncPC == 1 && enable ==1)
 			newPC <= newPC + 1;
+		else if (enable == 1)
+			newPC <= inputPC;
 	end
 				
 endmodule
